6722fec2b16dd0face808c001a61064d1b889129
T567U1 <f-dominguez1@outlook.com>
2020-03-22 23:24:05 -0400

    update

--- cov_19/views.py
+++ cov_19/views.py
@@ -8,9 +8,10 @@ def cov_19(request):
     url = 'https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6'
     browser = webdriver.PhantomJS(os.path.join("drivers/phantomjs-2.1.1-windows/bin/phantomjs.exe"))
     browser.get(url)
-    time.sleep(10)
+    time.sleep(20)
     page = browser.page_source
     soup = BeautifulSoup(page, 'lxml')
+    print(os.path)
 
     context = {
         'totalConfirmed' : soup.find(id='ember26').find_all('g')[3].text,
