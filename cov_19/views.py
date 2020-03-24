9eb23b72e30bb710ec531da86a30bfa9f64ac3e3
T567U1 <f-dominguez1@outlook.com>
2020-03-23 23:22:30 -0400

    it works on my machine

--- cov_19/views.py
+++ cov_19/views.py
@@ -5,26 +5,21 @@ import requests, time, lxml, os
 
 # Create your views here.
 def cov_19(request):
-    '''#local
-    url = 'https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6'
-    browser = webdriver.PhantomJS(os.path.join("bin/phantomjs.exe"))
-    browser.get(url)
-    time.sleep(20)
-    page = browser.page_source
-
-    '''
     chrome_options = webdriver.ChromeOptions()
+    #production
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    #driver = webdriver.Chrome(executable_path=r'C:/Webdrivers/chromedriver.exe')
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    driver.get("https://www.google.com")
+    #driver.get("https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6")
+    time.sleep(15)
     page = driver.page_source
     soup = BeautifulSoup(page, 'lxml')
+
     context = {
-        'test' : soup
-        #'totalConfirmed' : soup.find(id='ember26').find_all('g')[3].text,
-        #'deaths': soup.find(id='ember83').find_all('g')[3].text
+        'totalConfirmed' : soup.find(id='ember26').find_all('g')[3].text,
+        'deaths': soup.find(id='ember83').find_all('g')[3].text
     }
     return render(request, 'cov_19.html', context)
