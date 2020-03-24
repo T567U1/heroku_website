8c59b96b39b152969b3270d117e88279481e7bc2
T567U1 <f-dominguez1@outlook.com>
2020-03-23 21:52:16 -0400

    it works

--- cov_19/views.py
+++ cov_19/views.py
@@ -11,7 +11,7 @@ def cov_19(request):
     browser.get(url)
     time.sleep(20)
     page = browser.page_source
-    soup = BeautifulSoup(page, 'lxml')
+
     '''
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
@@ -20,9 +20,10 @@ def cov_19(request):
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get("https://www.google.com")
-    print(driver.page_source)
+    page = driver.page_source
+    soup = BeautifulSoup(page, 'lxml')
     context = {
-        'test' : 'hell'
+        'test' : soup
         #'totalConfirmed' : soup.find(id='ember26').find_all('g')[3].text,
         #'deaths': soup.find(id='ember83').find_all('g')[3].text
     }
