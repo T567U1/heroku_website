1b6e5df8f6ada9a0972811b2a44b194ffdacd0f4
T567U1 <f-dominguez1@outlook.com>
2020-03-24 00:32:07 -0400

    maybe closing will stop from crashing

--- cov_19/views.py
+++ cov_19/views.py
@@ -8,7 +8,7 @@ def cov_19(request):
     chrome_options = webdriver.ChromeOptions()
     #production
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    #driver = webdriver.Chrome(executable_path=r'C:/Webdrivers/chromedriver.exe')
+    driver = webdriver.Chrome(executable_path=r'C:/Webdrivers/chromedriver.exe')
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
@@ -22,4 +22,5 @@ def cov_19(request):
         'totalConfirmed' : soup.find(id='ember26').find_all('g')[3].text,
         'deaths': soup.find(id='ember83').find_all('g')[3].text
     }
+    driver.close()
     return render(request, 'cov_19.html', context)
