6ae4aadb5b58ce9283998adab86a4ec73cf4eeb6
T567U1 <f-dominguez1@outlook.com>
2020-03-23 23:42:30 -0400

    30 is too long

--- cov_19/views.py
+++ cov_19/views.py
@@ -14,7 +14,7 @@ def cov_19(request):
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     #driver.get("https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6")
-    time.sleep(30)
+    time.sleep(20)
     page = driver.page_source
     soup = BeautifulSoup(page, 'lxml')
 
