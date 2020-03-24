a2d1251a1f19fc72c6f693ee9700ca55ca725b4b
T567U1 <f-dominguez1@outlook.com>
2020-03-24 00:36:10 -0400

    Im disable

--- cov_19/views.py
+++ cov_19/views.py
@@ -8,7 +8,7 @@ def cov_19(request):
     chrome_options = webdriver.ChromeOptions()
     #production
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    driver = webdriver.Chrome(executable_path=r'C:/Webdrivers/chromedriver.exe')
+    #driver = webdriver.Chrome(executable_path=r'C:/Webdrivers/chromedriver.exe')
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
