e5d43bdf520b223ab41230ff3b7e12110f1cc9a0
T567U1 <f-dominguez1@outlook.com>
2020-04-12 22:22:11 -0400

    ip

--- home/views.py
+++ home/views.py
@@ -6,10 +6,10 @@ import requests
 def get_ip(request):
 
     ip = get_client_ip(request)
-    ip_info = requests.get(f'https://geo.ipify.org/api/v1?apiKey=at_FIfs0SjKrCBmADYEqiGrHYDakY1FP&ipAddress={{ip[0]}}')
+    ip_info = requests.get(f'https://geo.ipify.org/api/v1?apiKey=at_FIfs0SjKrCBmADYEqiGrHYDakY1FP&ipAddress={ip[0]}')
     ip_json = ip_info.json()
     get_ip_info = {
-        'location': ip_json['location']
+        'location': ip_json['ip']
     }
 
     return render(request, 'home.html', get_ip_info)
