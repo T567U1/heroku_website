21daadd75483ce751740cf1b9f821f7cc91840fb
T567U1 <f-dominguez1@outlook.com>
2020-04-12 23:15:36 -0400

    ip update

--- home/views.py
+++ home/views.py
@@ -4,12 +4,16 @@ import requests
 
 # Create your views here.
 def get_ip(request):
-
     ip = get_client_ip(request)
-    ip_info = requests.get(f'https://geo.ipify.org/api/v1?apiKey=at_FIfs0SjKrCBmADYEqiGrHYDakY1FP&ipAddress={ip[0]}')
+    try:
+        ip_info = requests.get(f'https://geo.ipify.org/api/v1?apiKey=at_FIfs0SjKrCBmADYEqiGrHYDakY1FP&ipAddress={ip[0]}')
+    except:
+        ip_info = requests.get(f'https://geo.ipify.org/api/v1?apiKey=at_FIfs0SjKrCBmADYEqiGrHYDakY1FP&ipAddress=8.8.8.8')
     ip_json = ip_info.json()
+
     get_ip_info = {
-        'location': ip_json['ip']
+        'ip': ip_json['location']['country'],
+        'test': 'hello',
     }
 
     return render(request, 'home.html', get_ip_info)
